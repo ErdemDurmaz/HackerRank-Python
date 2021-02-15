4b030665c9f536b223624d99cc79a647fba7f328
Erdem <245907@student.pwr.edu.pl>
2021-02-15 23:10:55 +0100

    0.33

--- leap year.py
+++ leap year.py
@@ -1,12 +1,15 @@
+'''
+The year can be evenly divided by 4, is a leap year, unless:
+The year can be evenly divided by 100, it is NOT a leap year, unless:
+The year is also evenly divisible by 400. Then it is a leap year.
+
+'''
+
 def is_leap(year):
     leap = False
-    if year % 4 ==0:
-        if year % 100 == 0:
-            if year /4 == 0:
-                return True
-        
+    if year % 4 ==0 and year % 400 ==0  and not year % 100 == 0:
+        return True
     # Write your logic here
-    
     return leap
 
 year = int(input())
