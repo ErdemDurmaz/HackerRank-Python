7bef548423b70765fc6cd464cc063a366b9ef924
Erdem <245907@student.pwr.edu.pl>
2021-03-24 18:25:47 +0100

    1.0

--- stringfinder.py
+++ stringfinder.py
@@ -3,7 +3,7 @@ def count_substring(string, sub_string):
     for i in range(len(string)):
         if string[i:].startswith(sub_string):
             count += 1
-    print(count)
+    return(count)
 
 count_substring("ABCDCDC","CDC")
 #for i in range(0, len(s)):
